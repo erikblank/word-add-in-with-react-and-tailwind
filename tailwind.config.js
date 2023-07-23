585ce4f93ec8fe0a4c0a754fb0e7cd04036c68aa
erikblank <erikblank97@web.de>
2023-07-23 21:46:51 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}", "./taskpane/taskpane.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1B73E8",
+      },
+    },
+  },
+  plugins: [],
+};
